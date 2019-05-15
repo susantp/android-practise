c629e1b1e0fffca48bbba86f02d43f618954f584
Susant Paudel <crupee.susant@gmail.com>
2019-05-15 15:58:03 +0545

    added menu inflator

--- app/src/main/java/com/example/fab/LoginActivity.java
+++ app/src/main/java/com/example/fab/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -62,5 +64,22 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.login_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.exit:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/fab/MainActivity.java
+++ app/src/main/java/com/example/fab/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -59,5 +61,22 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "UserInfo Saved", Toast.LENGTH_LONG).show();
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.login_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.exit:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
